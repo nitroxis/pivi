0063df6bc5c18ee145ea03d772325c4b415c9dc0
Maximilian Klein <dasmaxl@gmail.com>
2015-05-14 10:10:53 +0200

    (test) Test for lines, polylines and circles

--- test/commands.js
+++ test/commands.js
@@ -5,11 +5,11 @@ var chai = require("chai");
 chai.should();
 
 describe("Parsing Commands", function(){
-  it("parses points", function(){
+  it("parses a single point", function(){
     var point =
        ["point (1,2)",
         {
-            type:"point",
+            type:"points",
             data: [{
                 x: 1,
                 y: 2
@@ -18,11 +18,24 @@ describe("Parsing Commands", function(){
 
     parse(point[0]).should.deep.equal(point[1]);
   });
+  it("parses multiple points", function(){
+    var point =
+       ["points (1,2) 3 4",
+        {
+            type:"points",
+            data: [
+              {x: 1,y: 2},
+              {x: 3,y: 4}
+            ]
+        }];
+
+    parse(point[0]).should.deep.equal(point[1]);
+  });
   it("should ignore whitespaces", function(){
     var point =
         ["point  \t ( 10 , 0 )",
         {
-            type: "point",
+            type: "points",
             data: [{
                 x: 10,
                 y: 0
@@ -34,13 +47,79 @@ describe("Parsing Commands", function(){
     var point =
         ["point   ( -10 , -5 )",
         {
-            type: "point",
+            type: "points",
             data: [{
                 x: -10,
                 y: -5
             }]
         }];
     parse(point[0]).should.deep.equal(point[1]);
+  });
+  it("can parse one line", function(){
+    var line =
+        ["line -10 -5 10 10",
+        {
+            type: "lines",
+            data: [
+              {x: -10,y: -5},
+              {x:10,y:10}]
+        }];
+    parse(line[0]).should.deep.equal(line[1]);
+  });
+  it("can parse multiple lines", function(){
+    var lines =
+        ["lines -10 -5 10 10 0 0 20 20",
+        {
+            type: "lines",
+            data: [
+              {x:-10,y:-5},
+              {x:10,y:10},
+              {x:0,y:0},
+              {x:20,y:20}]
+        }];
+    parse(lines[0]).should.deep.equal(lines[1]);
+  });
+  it("can parse a polyline", function(){
+    var polyline =
+        ["polyline -10 -5 10 10 0 0 20 20",
+        {
+            type: "polyline",
+            data: [
+              {x:-10,y:-5},
+              {x:10,y:10},
+              {x:0,y:0},
+              {x:20,y:20}]
+        }];
+    parse(polyline[0]).should.deep.equal(polyline[1]);
+  });
+  it("parses a single circle", function(){
+    var circle =
+       ["circle (1,2) 5",
+        {
+            type:"circles",
+            data: [[{
+                x: 1,
+                y: 2
+            },5]]
+        }];
+
+    parse(circle[0]).should.deep.equal(circle[1]);
+  });
+  it("parses multiple circles", function(){
+    var circles =
+       ["circle (1,2) 5 2 2 10",
+        {
+            type:"circles",
+            data: [
+              [{x: 1,y: 2},5],
+              [{x: 2,y: 2},10]
+            ]
+        }];
+
+    parse(circles[0]).should.deep.equal(circles[1]);
+  });
+  it("should reject negative radii", function(){
+    (function(){parse("circle (1,2) -5");}).should.throw(Error);
   });
   it("detects Push commands", function(){
     parse("[").should.deep.equal({type:"push"});
