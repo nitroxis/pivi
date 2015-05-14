77891fc9cc4599ccc9194eded344d0f413be27a8
Maximilian Klein <dasmaxl@gmail.com>
2015-05-14 21:12:28 +0200

    (feature) Parse settings prefixed with "set"

--- test/properties.js
+++ test/properties.js
@@ -6,42 +6,40 @@ chai.should();
 describe("Parsing Commands", function(){
   it("can parse the 'pointSize' property", function(){
     var property =
-       ["pointSize 7",
+       ["set pointSize 7",
         {
             type:"property",
             property: "pointSize",
             data: 7
         }];
-        
+
     parse(property[0]).should.deep.equal(property[1]);
   });
   it("doesn't accept negative pointSizes", function(){
-    var pointSize = "pointSize -7";
-        
+    var pointSize = "set pointSize -7";
+
     (function(){parse(pointSize[0]);}).should.throw(Error);
   });
   it("can parse the 'color' property with an tupel", function(){
     var property =
-       ["color 128 200 100",
+       ["set color rgb 128 200 100",
         {
-            type:"property",
-            property: "color",
-            data: {x:128, y:200,z:100}
+            type: "color",
+            data: {x:128, y:200, z:100}
         }];
-        
+
     parse(property[0]).should.deep.equal(property[1]);
   });
   it("can parse the 'color' property using color names", function(){
     var knownColors = ["red","green","blue"];
     _.each(knownColors, function(c){
-      var property = ["color " + c,
+      var property = ["set color " + c,
         {
-            type:"property",
-            property: "color",
+            type: "color",
             data: c
         }];
         parse(property[0]).should.deep.equal(property[1]);
     });
-    
+
   })
 });
