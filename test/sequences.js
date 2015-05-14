93e8c81f210c55553b29bbf50d1be8f8e4f59be4
Maximilian Klein <dasmaxl@gmail.com>
2015-05-14 18:34:17 +0200

    (refactor) Describe commands via sequences
    Still WIP has some problems with repeating datasets for circles.

--- test/sequences.js
+++ test/sequences.js
@@ -8,33 +8,48 @@ var chai = require("chai");
 chai.should();
 
 describe("Command Sequences", function(){
-  it("it converts a command into a call descriptor", function(){
-    var seq = sequence(["function",["args"]])();
-    seq.should.deep.equal([["function",["args"]]]);
+  /*it("it converts a command into a call descriptor", function(){
+    var seq = sequence({call:"function",args:["args"]})();
+    seq.should.deep.equal([{call:"function",args:["args"]}]);
 
-    var seq = sequence(["function",["args"]], ["fn2",[]])();
-    seq.should.deep.equal([["function",["args"]],["fn2",[]]]);
+    var seq = sequence({call:"function",args:["args"]}, {call:"fn2",args:[]})();
+    seq.should.deep.equal([{call:"function",args:["args"]},{call:"fn2",args:[]}]);
   });
   it("applies data to each command", function(){
-    var seq = sequence(["function",["@x"]], ["fn2",["@y"]])([{x:0},{y:1}]);
-    seq.should.deep.equal([["function",[0]],["fn2",[1]]]);
+    var seq = sequence({call:"function",args:["@x"]}, {call:"fn2",args:["@y"]})([{x:0},{y:1}]);
+    seq.should.deep.equal([{call:"function",args:[0]},{call:"fn2",args:[1]}]);
   });
   it("can repeat one element", function(){
-    var seq = sequence(repeat(["function",["x"]]))([{},{},{}]);
+    var seq = sequence(repeat({call:"function",args:["x"]}))([{},{},{}]);
     seq.length.should.equal(3);
   });
   it("returns an empty set for repeating no data", function(){
-    var seq = sequence(repeat(["function",["x"]]))([]);
+    var seq = sequence(repeat({call:"function",args:["x"]}))([]);
     seq.should.deep.equal([]);
   });
   it("can switch between commands and repeating commands", function(){
-    var seq = sequence(["fn",[]],repeat(["fn",[]]))();
+    var seq = sequence({call:"fn",args:[]},repeat({call:"fn2",args:[]}))();
+    seq[1].call.should.equal("fn2");
     seq.length.should.equal(2);
   });
   it("cycles between multiple commands", function(){
-    var seq = sequence(cycle(["fn1",[]],["fn2",[]]))(Array(6));
+    var seq = sequence(cycle({call:"fn1",args:[]},{call:"fn2",args:[]}))(Array(6));
     seq.length.should.equal(6);
-    seq[4][0].should.equal("fn1");
-    seq[5][0].should.equal("fn2");
+    seq[4].call.should.equal("fn1");
+    seq[5].call.should.equal("fn2");
+  });
+  it("can repeat sequences", function(){
+    var seq = sequence(
+      repeat(
+        sequence({call:"fn1",args:[]},{call:"fn2",args:[]})))(Array(5));
+    seq.length.should.equal(10);
+  });*/
+  it("repeated sequences use data", function(){
+    var seq = sequence(
+      repeat(
+        sequence({call:"fn1",args:[]},{call:"fn2",args:["@a","@b"]})))([{a:0,b:1}]);
+    console.log(seq);
+    seq[1].args[0].should.equal(0);
+    seq[1].args[1].should.equal(1);
   });
 });
