ca637a2e7cad6343a34b37c64efc802df0a842ed
Maximilian Klein <dasmaxl@gmail.com>
2015-05-14 20:55:40 +0200

    (feature) Add support for setting the output filename

--- pivi.js
+++ pivi.js
@@ -4,8 +4,14 @@ var canvas = require("canvas");
 var es = require("event-stream");
 var parse = require("./lib/grammar.js").parse;
 var sc = require("./lib/streamCanvas.js");
+var argv = require("yargs")
+          .usage("echo 'line (0 0) (200 200)' | $0 [options]")
+          .default("f","./out.png")
+          .describe("f", "The output file that gets generated")
+          .help("h")
+          .argv;
 
 process.stdin
     .pipe(es.split())
     .pipe(es.mapSync(parse))
-    .pipe(sc());
+    .pipe(sc(argv.f));
