f2a8cd7c478d7a4894d9e411b27a9844125386cb
Maximilian Klein <dasmaxl@gmail.com>
2015-05-18 23:00:42 +0200

    (feature) Process animations with "newframe"
    Added the keyword "newframe" that ends the current frame and starts drawing the new frame.
    Also added an option for the canvas size

--- pivi.js
+++ pivi.js
@@ -6,7 +6,7 @@ var parse = require("./lib/grammar.js").parse;
 var sc = require("./lib/streamCanvas.js");
 var argv = require("yargs")
           .usage("echo 'line (0 0) (200 200)' | $0 [options]")
-          .default("f","./out.png")
+          .default("f","./out%d.png")
           .describe("f", "The output file that gets generated")
           .help("h")
           .argv;
@@ -14,4 +14,5 @@ var argv = require("yargs")
 process.stdin
     .pipe(es.split())
     .pipe(es.mapSync(parse))
-    .pipe(sc(argv.f));
+    .pipe(sc(argv.f))
+    .pipe(process.stdout);
